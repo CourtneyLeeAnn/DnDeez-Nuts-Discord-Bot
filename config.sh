ff2f7f02d8de5793ddba58fb4b063d713a8e60a1
RJ Hill <raymondjohnhill@gmail.com>
2023-06-04 19:07:30 -0400

    Adding some config stuff

---
+++ config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# read the file into a variable
+config_template=$(<template.config.json)
+
+# substitute all placeholders with corresponding environment variables
+for var in $(compgen -e); do
+    config_template=${config_template//\{\{$var\}\}/${!var}}
+done
+
+# write back the processed template into config.json
+echo "$config_template" > config.json
